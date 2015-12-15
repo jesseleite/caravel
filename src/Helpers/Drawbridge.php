3d8de8e9b3211778adec74a8072af1e282f052a4
Jesse Leite <jesse@thisvessel.co>
2015-12-15 18:35:01 -0500

    Cleaning up Drawbridge.

--- src/Helpers/Drawbridge.php
+++ src/Helpers/Drawbridge.php
@@ -6,43 +6,17 @@ use Gate;
 
 class Drawbridge
 {
-    protected static function model($arguments)
-    {
-        return is_array($arguments) ? $arguments[0] : $arguments;
-    }
-
-    protected static function reviseAbility($ability, $model)
-    {
-        $resource = array_search(get_class($model), config('caravel.resources'));
-
-        if (in_array($ability, ['update', 'delete'])) {
-            $resource = str_singular($resource);
-        }
-
-        return "$ability-$resource";
-    }
-
-    protected static function hasPolicy($policy, $arguments)
-    {
-        $model = static::model($arguments);
-
-        try {
-            $policyClass = Gate::getPolicyFor($model);
-            $hasPolicy = method_exists($policyClass, $policy);
-        } finally {
-            return isset($hasPolicy) ? $hasPolicy : false;
-        }
-    }
-
-    protected static function hasAbility($ability, $arguments)
-    {
-        return Gate::has($ability);
-    }
-
+    /**
+     * Check for policy or ability definition, otherwise skip authorization check and allow user.
+     *
+     * @param  string  $ability
+     * @param  mixed   $arguments
+     * @return boolean
+     */
     public static function allows($ability, $arguments = [])
     {
         $model = static::model($arguments);
-        $revisedAbility = static::reviseAbility($ability, $model);
+        $revisedAbility = static::explicitAbility($ability, $model);
 
         if (static::hasPolicy($ability, $arguments)) {
             return Gate::allows($ability, $arguments);
@@ -53,26 +27,87 @@ class Drawbridge
         return true;
     }
 
+    /**
+     * Check for policy or ability definition, otherwise skip authorization check and allow user.
+     *
+     * @param  string  $ability
+     * @param  mixed   $arguments
+     * @return boolean
+     */
     public static function denies($ability, $arguments = [])
     {
-        $model = static::model($arguments);
-        $revisedAbility = static::reviseAbility($ability, $model);
-
-        if (static::hasPolicy($ability, $arguments)) {
-            return Gate::denies($ability, $arguments);
-        } elseif (static::hasAbility($revisedAbility, $arguments)) {
-            return Gate::denies($revisedAbility, $arguments);
-        }
-
-        return false;
+        return ! static::allows($ability, $arguments);
     }
 
+    /**
+     * Check for policy or ability definition, otherwise skip authorization check and allow user.
+     *
+     * @param  string  $ability
+     * @param  mixed   $arguments
+     * @return mixed
+     */
     public static function authorize($ability, $arguments = [])
     {
-        if (static::denies($ability, $arguments)) {
-            return abort(403, 'You shall not pass!');
+        return static::allows($ability, $arguments) ? true : abort(403, 'You shall not pass!');
+    }
+
+    /**
+     * Grab model from arguments.
+     *
+     * @param  mixed  $arguments
+     * @return \Illuminate\Database\Eloquent\Model;
+     */
+    protected static function model($arguments)
+    {
+        return is_array($arguments) ? $arguments[0] : $arguments;
+    }
+
+    /**
+     * Revise ability to include explicit resource name.
+     *
+     * @param  string  $ability
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return string
+     */
+    protected static function explicitAbility($ability, $model)
+    {
+        $resource = array_search(get_class($model), config('caravel.resources'));
+
+        if (in_array($ability, ['update', 'delete'])) {
+            $resource = str_singular($resource);
         }
 
-        return true;
+        return "$ability-$resource";
+    }
+
+    /**
+     * Check if ability has been defined.
+     *
+     * @param  string  $ability
+     * @param  mixed   $arguments
+     * @return boolean
+     */
+    protected static function hasAbility($ability, $arguments)
+    {
+        return Gate::has($ability);
+    }
+
+    /**
+     * Check if policy has been defined.
+     *
+     * @param  string  $policy
+     * @param  mixed   $arguments
+     * @return boolean
+     */
+    protected static function hasPolicy($policy, $arguments)
+    {
+        $model = static::model($arguments);
+
+        try {
+            $policyClass = Gate::getPolicyFor($model);
+            $hasPolicy = method_exists($policyClass, $policy);
+        } finally {
+            return isset($hasPolicy) ? $hasPolicy : false;
+        }
     }
 }
