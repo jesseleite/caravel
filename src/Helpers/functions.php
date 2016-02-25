345ab0a95a0e91d0f42be95a8bf0639198cf4c96
Jesse Leite <jesse.leite@roomroster.com>
2016-02-24 22:03:14 -0500

    Adding caravel_route() helper function.

--- src/Helpers/functions.php
+++ src/Helpers/functions.php
@@ -1,5 +1,17 @@
 <?php
 
-function caravel_test() {
-    return 'wat';
+if (! function_exists('caravel_route')) {
+    /**
+     * Get caravel route for a model action.
+     *
+     * @param  string  $action
+     * @param  mixed  $model
+     * @return string
+     */
+    function caravel_route($action, $model)
+    {
+        $resource = collect(config('caravel.resources'))->search(get_class($model));
+
+        return route("caravel::{$resource}.$action", $model);
+    }
 }
