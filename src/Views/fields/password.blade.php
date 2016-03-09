825b4ff1740536eec1579f61e9c25166939dc222
Jesse Leite <jesse.leite@roomroster.com>
2016-03-08 23:53:11 -0500

    If input type:password is used with 'confirmed' rule, add confirmation input.

--- src/Views/fields/password.blade.php
+++ src/Views/fields/password.blade.php
@@ -1,3 +1,8 @@
 <!-- Password Input -->
 {!! $bootForm->password($field->label, $field)
              ->helpBlock($field->help) !!}
+
+@if (str_contains($field->rules, 'confirmed'))
+    {!! $bootForm->password("{$field->label} Confirmation", "{$field}_confirmation")
+                 ->helpBlock($field->help) !!}
+@endif
